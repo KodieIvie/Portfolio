b2d5d247557f1d69f81cfd5751b64a5f8f757572
kodie <fxit85@users.noreply.github.com>
2018-04-13 11:20:04 -0600

    fixed keys for public

---
+++ config/keys.js
@@ -0,0 +1,17 @@
+module.exports = {
+	gmail: {
+		user: 'dummyemaildemo',
+        pass: 'omedliame'
+	},
+	google: {
+		clientID: '239644147869-a1vbssnd4mjune9c9vc4sv9flvisuc2k.apps.googleusercontent.com',
+		clientSecret: 'o85Ov2nLgX8AkqDE2NniBNb1'
+	},
+	session: {
+		cookieKey: 'Life would be tragic if it weren\'t funny.'
+	},
+	db: {
+		mlab: 'mongodb://kodiei:Summers21@ds119129.mlab.com:19129/portfolio'
+	},
+	news: '7a6e92d8fbe3410eac0ccc36562806e4'
+}
